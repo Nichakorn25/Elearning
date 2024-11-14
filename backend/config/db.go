88f1725327919d83c0d50d6fffa8dea0dd91be84
Bam1q <boomvs4769@gmail.com>
2024-11-14 20:48:37 +0700

    เพิ่ม db.go, middlewares, services
    db.go ยังไม่
    middlewares, service สมบูรณ์

---
+++ backend/config/db.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"fmt"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func DB() *gorm.DB {
+	return db
+}
+
+func ConnectionDB() {
+	database, err := gorm.Open(sqlite.Open("sa.db?cache=shared"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("connected database")
+	db = database
+}
+
+// Set Database เริ่มต้น
+func SetupDatabase() {
+
+}
