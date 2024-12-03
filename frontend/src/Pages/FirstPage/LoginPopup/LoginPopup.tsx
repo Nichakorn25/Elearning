a25db4c4977058237359c6e9d2f46e029201e723
Nichakorn Chanyutha <nichakorn391@gmail.com>
2024-12-03 23:22:50 +0700

    แก้ LoginPopup

--- frontend/src/Pages/FirstPage/LoginPopup/LoginPopup.tsx
+++ frontend/src/Pages/FirstPage/LoginPopup/LoginPopup.tsx
@@ -17,12 +17,12 @@ const LoginPopup: React.FC<LoginPopupProps> = ({ onClose }) => {
     let res = await SignIn(values);
 
     if (res.status === 200) {
-      messageApi.success("Sign-in Successful");
+      messageApi.success('Sign-in Successful');
 
-      localStorage.setItem("isLogin", "true");
-      localStorage.setItem("token_type", res.data.token_type);
-      localStorage.setItem("token", res.data.token);
-      localStorage.setItem("id", res.data.id);
+      localStorage.setItem('isLogin', 'true');
+      localStorage.setItem('token_type', res.data.token_type);
+      localStorage.setItem('token', res.data.token);
+      localStorage.setItem('id', res.data.id);
 
       const userId = res.data.id;
       let userResponse = await GetUserById(userId);
@@ -32,20 +32,18 @@ const LoginPopup: React.FC<LoginPopupProps> = ({ onClose }) => {
 
         setTimeout(() => {
           if (user.RoleID === 1) {
-            message.success("You are a student!");
-            navigate("/Dashboard");
-          } 
-          else if (user.RoleID === 2) {
-            message.success("You are a teacher!");
-            navigate("/Dashboard");
-          } 
-          else if (user.RoleID === 3)  {
-            message.success("You are an Admin!")
-            navigate("/Dashboard");
+            message.success('You are a student!');
+            navigate('/Dashboard');
+          } else if (user.RoleID === 2) {
+            message.success('You are a teacher!');
+            navigate('/Dashboard');
+          } else if (user.RoleID === 3) {
+            message.success('You are an Admin!');
+            navigate('/Dashboard');
           }
         }, 1000);
       } else {
-        messageApi.error("Failed to retrieve user data");
+        messageApi.error('Failed to retrieve user data');
       }
     } else {
       messageApi.error(res.data.error);
@@ -56,40 +54,51 @@ const LoginPopup: React.FC<LoginPopupProps> = ({ onClose }) => {
     <div className="popup-overlay" onClick={onClose}>
       {contextHolder}
       <div className="popup-container" onClick={(e) => e.stopPropagation()}>
-        <h2>SUT e-Learning</h2>
+        <h2 className="popup-title">SUT e-Learning</h2>
         <Form
           name="login"
           onFinish={onFinish}
           className="popup-form"
           requiredMark={false}
         >
-          <Form.Item
-            name="username"
-            rules={[{ required: true, message: 'Please enter your Username!' }]}
-          >
-            <Input placeholder="Username" />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: 'Please enter your Password!' }]}
-          >
-            <Input.Password placeholder="Password" />
-          </Form.Item>
-          <Form.Item>
-            <button className="popup-login-button" type="submit">
-              เข้าสู่ระบบ
-            </button>
-          </Form.Item>
+          <div className="input-box">
+  <Input
+    type="text"
+    name="username"
+    placeholder="Username"
+    className="input-field"
+  />
+  <i className="bx bxs-user input-icon"></i> {/* ไอคอนคน */}
+</div>
+<div className="input-box">
+  <Input.Password
+    type="password"
+    name="password"
+    placeholder="Password"
+    className="input-field"
+  />
+  <i className="bx bxs-lock-alt input-icon"></i> {/* ไอคอนกุญแจ */}
+</div>
+
+          <div className="remember-forgot-box">
+            <div className="remember-me">
+              <Input type="checkbox" id="remember-me" />
+              <label htmlFor="remember-me">Remember me</label>
+            </div>
+            <a href="#" className="forgot-password">
+              Forgot password?
+            </a>
+          </div>
+          <button className="popup-login-button" type="submit">
+            Login
+          </button>
+          <div className="dont-have-account">
+            Don't have an account?{' '}
+            <a href="#" onClick={(e) => e.preventDefault()}>
+              Register
+            </a>
+          </div>
         </Form>
-        <div className="popup-links">
-          <a href="#" onClick={(e) => { e.preventDefault(); onClose(); navigate('/ForgotPassword'); }}>
-            ลืมรหัสผ่าน
-          </a>{' '}
-          |{' '}
-          <a href="#" onClick={(e) => { e.preventDefault(); onClose(); navigate('/SignUp'); }}>
-            สมัครสมาชิก
-          </a>
-        </div>
       </div>
     </div>
   );
